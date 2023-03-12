ee289076a50159708a08454adec71f93f10d735c
Bruno Pontiz <bpontiz@gmail.com>
2023-03-12 11:17:12 -0300

    Creating SignUp Component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navigation from './Components/Navigation/Navigation.jsx';
 import Login from './Components/Login/Login.jsx';
+import Register from './Components/Register/Register.jsx';
 
 function App() {
   return (
     <Router>
       <Navigation />
-      <Login />
+      <Register />
       <Routes>
-        <Route path='/' element={<Login />}></Route>
+        <Route path='/' element={<Register />}></Route>
       </Routes>
     </Router>
     
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,14 +1,13 @@
-import './Login.css';
+import './Register.css';
 import { useState } from 'react';
 
-export default function Login() {
-
+export default function Register() {
     const [password, setPassword] = useState("password")
 
     return (
         <div className='divForm'>
             <form action='http://localhost:8081/api/users' method="post" autoComplete='off' className='loginForm'>
-                <p className='login'>Log In</p>
+                <p className='login'>Sign Up</p>
                 <label htmlFor="username" className='labels'>Username</label>
                 <br />
                 <input type="text" name='username' className='inputBox' required/>
@@ -16,6 +15,14 @@ export default function Login() {
                 <label htmlFor="password" className='labels'>Password</label>
                 <br />
                 <input type={password} name='password' className='inputBox' id='passwordBox' required/>
+                <br />
+                <label htmlFor="adress" className='labels'>Adress</label>
+                <br />
+                <input type="text" name='adress' className='inputBox' required/>
+                <br />
+                <label htmlFor="email" className='labels'>Email</label>
+                <br />
+                <input type="email" name='email' className='inputBox' required/>
                 <br />
                 <div className="divPass">
                     <input type="checkbox" name="checkbox" value='Show Password' 
