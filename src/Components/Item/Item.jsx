d6af59ba63eac7852497de04fff54ce9d9e3195b
Bruno Pontiz <bpontiz@gmail.com>
2023-04-02 11:21:44 -0300

    Add to cart functionality has been added to its respective button on ItemListContainer Component.

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,6 +1,25 @@
 import './Item.css';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Item( {id, name, price, stock, image, details} ) {
+    async function addToCartHandler() {
+        const getUserData = await axios.get('http://localhost:8081/api/users/auth/login');
+        const quantity = 1;
+        console.log(getUserData.data);
+
+        const newCart = {
+            email: getUserData.data.email,
+            items: {
+                name, price, quantity, image, details
+            },
+            address: getUserData.data.address
+        };
+        
+        return await axios.post('http://localhost:8081/api/carts', newCart);
+    };
+
+
     return (
         <div className='itemProductDiv'>
             <div className='itemProductImageDiv'>
@@ -11,7 +30,7 @@ export default function Item( {id, name, price, stock, image, details} ) {
             <p className="itemProductStock">Stock: {stock}</p>
             <p className="itemProductDetails">{details}</p>
             <div className='itemButtonsDiv'>
-                <button className='itemButton'>Add to cart</button>
+                <button className='itemButton' onClick={addToCartHandler}><Link to='/cart' className='linkAddToCart'>Add to cart</Link></button>
                 <button className='itemButton'>Buy</button>
             </div>
         </div>
