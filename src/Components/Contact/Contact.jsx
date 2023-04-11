0a1537129f5a55d0b8dc1eb66b16f5e6d83d5e88
Bruno Pontiz <bpontiz@gmail.com>
2023-04-11 20:45:02 -0300

    Contact module has been updated with a bug fix.

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,8 +2,6 @@ import './Contact.css';
 import io from 'socket.io-client';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import noUserIcon from '../../Multimedia/noUserIcon.png';
-import { Link } from 'react-router-dom';
 
 const socket = io('http://localhost:8081');
 
@@ -44,40 +42,30 @@ export default function Contact() {
         socket.on('message', receiveMessage);
     }, [messages]);
 
-    if(localStorage.getItem('username')) {
-        return(
-            <section className='sectionContact'>
-                <div className="divContact">
-                    <p className='pLeaveMessage'>Please leave us your message!</p>
     
-                    <div className="divAllMessages">
-                        {messages.map((message, index) => (
-                            <div className={message.from === "Me" ? 'divMessageChatMe' : 'divMessageChat'} key={index}>
-                                <p className={message.from === "Me" ? 'pMessageChatMe' : 'pMessageChat'}><strong>{message.from}:</strong> {message.body}</p>
-                            </div>
-                        ))}
-                    </div>
-    
-                    <form onSubmit={handleSubmit} className='formContact'>
-                        <input className='inputEnterMessage' onChange={(msg) => setMessage(msg.target.value)} type="text" value={message} placeholder='Enter message' required />
-                        <button type="submit" className='buttonFormContact'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send-fill" viewBox="0 0 16 16">
-                                <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z"/>
-                            </svg>
-                        </button>
-                    </form>
-                    
-                </div>
-            </section>
-        )
-    }
-
     return(
-        <section className="sectionContactNoUser">
-            <p className="pContactNoUserOops">Oops!</p>
-            <img className='imgContactNoUser' src={noUserIcon} alt="Undefined user" />
-            <Link to='/registered' className='linkContactNoUser'><p className='pContactNoUser'>Please log in first →</p></Link>
+        <section className='sectionContact'>
+            <div className="divContact">
+                <p className='pLeaveMessage'>Please leave us your message!</p>
+
+                <div className="divAllMessages">
+                    {messages.map((message, index) => (
+                        <div className={message.from === "Me" ? 'divMessageChatMe' : 'divMessageChat'} key={index}>
+                            <p className={message.from === "Me" ? 'pMessageChatMe' : 'pMessageChat'}><strong>{message.from}:</strong> {message.body}</p>
+                        </div>
+                    ))}
+                </div>
+
+                <form onSubmit={handleSubmit} className='formContact'>
+                    <input className='inputEnterMessage' onChange={(msg) => setMessage(msg.target.value)} type="text" value={message} placeholder='Enter message' required />
+                    <button type="submit" className='buttonFormContact'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send-fill" viewBox="0 0 16 16">
+                            <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z"/>
+                        </svg>
+                    </button>
+                </form>
+                
+            </div>
         </section>
     )
-    
 };
